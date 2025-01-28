336bff91e74e93ac80bc4d39d7fd4c9b50270ac6
tresor@foxkiradev <tresormakosso510@gmail.com>
2025-01-28 15:31:35 +0100

    ok

---
+++ TypeScript/task_1/js/main.ts
@@ -0,0 +1,27 @@
+interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [key: string]: any;
+}
+
+const teacher1: Teacher =  {
+    firstName: "john",
+    lastName: "Doe"
+    fullTimeEmployee: true,
+    location: "New York", 
+};
+
+const teacher2: Teacher = {
+    firstName: "jane",
+    lastName: "Smith",
+    fullTimeEmployee: false,
+    yearsOfExperience: 5,
+    location: "Los Angeles"
+    contract: true,
+};
+
+console.log(teacher1);
+console.log(teacher2);
