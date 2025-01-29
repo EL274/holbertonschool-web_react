8a47d792f94f0fc3210943c5f185fb16f44e19af
tresor@foxkiradev <tresormakosso510@gmail.com>
2025-01-29 03:02:51 +0100

    ok

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -25,3 +25,29 @@ const teacher2: Teacher = {
 
 console.log(teacher1);
 console.log(teacher2);
+
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+const director1: Directors = {
+    firstName: "john",
+    lastName: "string",
+    fullTimeEmployee: boolean,
+    location: "string",
+    numberOfReports: 17,     
+};
+
+const director2: Directors = {
+    firstName: "string",
+    lastName: "string",
+    fullTimeEmployee: boolean,
+    yearsOfExperience: "number",
+    location: "string",
+    numberOfReports: number,
+    contract: boolean,
+};
+
+console.log(director1);
+console.log(director2);
+
