9c64d57778fdc344d519425b089c60c9f003ba57
tresor@foxkiradev <tresormakosso510@gmail.com>
2025-01-29 15:53:18 +0100

    ok

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -72,3 +72,14 @@ interface DirectorInterface {
       employee.workTeacherTasks();
     }
   }
+
+  type Subjects = "Math" | "History"; 
+
+function teachClass(todayClass: Subjects): string { 
+    if (todayClass === "Math") {
+        return "Teaching Math";
+    } else {
+        return "Teaching History";
+    }
+}
+
