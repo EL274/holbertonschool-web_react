7ca48f5a05a5c264d5e4aa699561a8942fc48636
tresor@foxkiradev <tresormakosso510@gmail.com>
2025-01-30 16:53:51 +0100

    ok

--- TypeScript/task_3/js/crud.js
+++ TypeScript/task_3/js/crud.js
@@ -1,1 +1,11 @@
-import * as CRUD './js/crud';
+export function insertRow(row) {
+    console.log('insertRow'.row);
+    return math.floor(Math.random() * Math.floor(100));
+}
+
+export function deleteRow(rowid) {
+    console.log('DeleteRow'.row);
+    return;
+}
+
+export function updatedRow(rowid)
